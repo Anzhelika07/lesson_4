9d014d945bab415bed380e3f070e307bc3715751
Пользователь <anjela_karlova@mail.ru>
2025-07-28 20:57:29 +0700

    Сorrection numbers_average.py

--- numbers_average.py
+++ numbers_average.py
@@ -7,18 +7,21 @@ numbers_count = 0
 even_numbers_sum = 0
 even_numbers_count = 0
 
-while start_number <= end_number:
-    numbers_sum += start_number
-    numbers_count += 1
+if start_number < end_number:
+    while start_number <= end_number:
+        numbers_sum += start_number
+        numbers_count += 1
 
-    if start_number % 2 == 0:
-        even_numbers_sum += start_number
-        even_numbers_count += 1
+        if start_number % 2 == 0:
+            even_numbers_sum += start_number
+            even_numbers_count += 1
 
-    start_number += 1
+        start_number += 1
 
-even_numbers_average = even_numbers_sum / even_numbers_count
-print(f"Среднее арифметическое чётных чисел = {even_numbers_average}")
+    even_numbers_average = even_numbers_sum / even_numbers_count
+    print(f"Среднее арифметическое чётных чисел = {even_numbers_average}")
 
-numbers_average = numbers_sum / numbers_count
-print(f"Среднее арифметическое чисел = {numbers_average}")
+    numbers_average = numbers_sum / numbers_count
+    print(f"Среднее арифметическое чисел = {numbers_average}")
+else:
+    print("Начальное число должно быть меньше, чем конечное число. Попробуйте ввести ещё раз.")
